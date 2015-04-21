54523631f750c7016a052d7181b8517429775d8b
Julia Rose <julmrose@gmail.com>
2015-04-21 16:27:07 -0400

    pushing app to heroku for user testing

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150421181554) do
-=======
-ActiveRecord::Schema.define(version: 20150421143730) do
->>>>>>> uhhlittle-master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
-=======
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -40,7 +34,6 @@ ActiveRecord::Schema.define(version: 20150421143730) do
   add_index "installs", ["email"], name: "index_installs_on_email", unique: true, using: :btree
   add_index "installs", ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true, using: :btree
 
->>>>>>> uhhlittle-master
   create_table "states", force: :cascade do |t|
     t.string   "name"
     t.boolean  "grow"

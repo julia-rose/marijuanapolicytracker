85cb561fc64d3e90aa7d15a8a5956c9c4646977c
Julia Rose <julmrose@gmail.com>
2015-04-21 11:47:28 -0400

    working on state show page, added figaro and unirest gems

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,2 +1,19 @@
 class StatesController < ApplicationController
+
+	def index
+		@states = State.all
+	end
+
+	def show
+		secret_key = ENV['secret_key']
+		@state = State.find(params[:id])
+
+		@response = Unirest.get("https://faroo-faroo-web-search.p.mashape.com/api?q=marijuana+#{@state.name}",
+  		headers:{
+		    "X-Mashape-Key" => "#{secret_key}",
+		    "Accept" => "application/json"
+		  }).body
+
+
+	end
 end

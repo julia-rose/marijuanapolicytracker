1a7f346db3944b4de88ad64a4dc022b054a0933e
Julia Rose <julmrose@gmail.com>
2015-04-22 14:23:59 -0400

    css fixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
+
 ActiveRecord::Schema.define(version: 20150422175221) do
-=======
-ActiveRecord::Schema.define(version: 20150421181554) do
->>>>>>> DamonMd-master
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

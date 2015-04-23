489e5fd095aea8f384165b76abab06fa7a16af2d
Julia Rose <julmrose@gmail.com>
2015-04-23 17:24:06 -0400

    css css css

--- db/migrate/20150423210939_create_states.rb
+++ db/migrate/20150423210939_create_states.rb
@@ -1,7 +1,7 @@
 class CreateStates < ActiveRecord::Migration
   def change
     create_table :states do |t|
-    	    	t.string :name
+    	t.string :name
     	t.string :possession
     	t.text :possession_details
     	t.string :paraphernalia
